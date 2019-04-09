9642df4e1fc70fe2cc1f714dc101198c1d13798c
Aaron Whitlock <whitlock.aaron@yahoo.com>
2019-04-08 22:41:13 -0500

    added caesar code and route to catch input

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, request
+from caesar import rotate_string
 import cgi
 
 app = Flask(__name__)
@@ -10,35 +11,47 @@ page_header = """<!DOCTYPE html>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <style> form {background-color: #eee;
+    <title>Web-Caesart</title>
+    <style> 
+    form {
+        background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
-                border-radius: 10px; } textarea { margin: 10px 0;
+                border-radius: 10px; } 
+        textarea { margin: 10px 0;
                 width: 540px;
-                height: 120px;}</style>
+                height: 120px;}
+                </style>
 </head>
 <body>"""
 
 page_footer = """</body>
 </html>"""
 
-form = page_header+ """<form method = 'POST'> 
+form =  """<form method = 'POST'> 
         <label for = "rot">Rotate by</label>
 
         <input type ="text" name = "rot" value ="0"/>
 
         <label for = "text">Enter text</label>
-        <input type = "text" name ="text" />
+        <input type = "text" name ="text" id = "textarea" />
         <input type = "submit" value = "send that thing" />
-    </form>"""+page_footer
+    </form>"""
 
 @app.route("/")
 def index():
-    return form
+    return page_header+form+page_footer
 
+@app.route("/", methods=['POST'])
+def encrypt():
+
+    rot = int(request.form['rot'])
+    text = request.form['text']
+    new_string = rotate_string(text, rot)
+    print(new_string)
+    return """<h1>{0}</h1>""".format(new_string)
 
 
 
